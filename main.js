310d4261cb51606dcfe3a57d17b211e0a9b0de52
HawkEyeVe <deutsch.escalante@gmail.com>
2024-12-20 17:37:40 -0400

    add dice as funcion and add 3 mage fighter and ranger

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+function diceType (dice) {
+    dice= dice.slice(1)
+    dice = Number(dice)
+    dice = Math.floor(Math.random() * (dice + 1));
+    return dice
+}
+
 class Charecters {
     constructor(_name, _age) {
         this.name = _name;
@@ -12,9 +19,9 @@ class Charecters {
             let  tempDice = 0
     
             for (let j = 0; j < 4; j++){
-                tempDice = Math.floor(Math.random() * 6) + 1
+                tempDice = diceType("d6")
                     if (tempDice <=1 ){
-                        Math.floor(Math.random() * 6) + 1
+                        diceType("d6")
                 }
                 dice.push(tempDice);
             }
@@ -31,6 +38,17 @@ class Charecters {
             this.mod.push(Math.floor((this.attributes[i] - 10) / 2));
         }
     }
+
+    setCharacter() {
+        this.createAttributes();
+        this.calculateMod();
+        const char = {
+            attributes : this.attributes,
+            mod : this.mod
+        }
+        console.table(char)
+        return char
+    }
 }
 
 class mage extends Charecters {
@@ -39,29 +57,69 @@ class mage extends Charecters {
         this.hp = 0
         this.spells = [];
     }
-    setCharacter() {
-        super.createAttributes();
-        super.calculateMod();
-        const char = {
-            attributes : this.attributes,
-            mod : this.mod
-        }
-        console.table(char)
-        return char
-    }
 
-    char = this.setCharacter();
+
+    char = super.setCharacter();
 
     hpCalc() {
         this.hp = 6 + this.char.mod[0]
         for (let i = 0; i <=2; i++) {
-            this.hp += Math.floor(Math.random() * 6) + 1 + this.char.mod[0]
+            this.hp += diceType("d6")
+        }
+    }
+
+}
+
+class ranger extends Charecters {
+    constructor(name, age) {
+        super(name, age);
+        this.hp = 0
+        this.spells = [];
+    }
+
+
+    char = super.setCharacter();
+
+    hpCalc() {
+        this.hp = 6 + this.char.mod[0]
+        for (let i = 0; i <=2; i++) {
+            this.hp += diceType("d8")
+        }
+    }
+
+}
+
+class fighter extends Charecters {
+    constructor(name, age) {
+        super(name, age);
+        this.hp = 0
+        this.spells = [];
+    }
+
+
+    char = super.setCharacter();
+
+    hpCalc() {
+        this.hp = 6 + this.char.mod[0]
+        for (let i = 0; i <=2; i++) {
+            this.hp += diceType("d10")
         }
     }
 
 }
 
 
+
 const simon = new mage("simon", 20);
+console.log(simon.name)
 simon.hpCalc();
-console.log(simon.hp)
+console.log(`Hp ${simon.hp}`)
+const diego = new ranger("diego", 30);
+console.log(diego.name)
+diego.hpCalc();
+console.log(`Hp ${diego.hp}`)
+const cara = new fighter("cara", 40);
+console.log(cara.name)
+cara.hpCalc();
+console.log(`Hp ${cara.hp}`)
+
